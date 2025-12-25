164def343a3021e493cb3c99a1566c84e555a2ed
Dongwan Kim <dongwan204@naver.com>
2025-12-25 22:30:07 +0900

    Feature: Added utilities
    - History utility
    - Data analyzing tools

---
+++ src/main/java/org/neatore/calisync/util/Analyze.java
@@ -0,0 +1,37 @@
+package org.neatore.calisync.util;
+
+import org.neatore.calisync.CaliSync;
+import org.neatore.calisync.object.Date;
+import org.neatore.calisync.object.Schedule;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Analyze {
+    public static List<Schedule> getContents(ResultSet rs) {
+        final List<Schedule> result = new ArrayList<>();
+        try {
+            String it_unique_id = rs.getString("it_unique_id").replace("dkcal_mdays_", "");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            LocalDate date = LocalDate.parse(it_unique_id, formatter);
+
+            String content = rs.getString("it_content");
+            Scanner scan = new Scanner(content);
+
+            while (scan.hasNext()) {
+                // TODO: 취소선으로 isCompleted 판단하기
+                result.add(new Schedule(new Date(Integer.toString(date.getYear()), Integer.toString(date.getMonthValue()), Integer.toString(date.getDayOfMonth())), scan.nextLine(), false));
+            }
+        } catch (SQLException e) {
+            CaliSync.LOGGER.error(e);
+        }
+        return result;
+    }
+}
--- src/main/java/org/neatore/calisync/util/HistoryParser.java
+++ src/main/java/org/neatore/calisync/util/HistoryParser.java
@@ -1,16 +1,50 @@
 package org.neatore.calisync.util;
 
+import org.neatore.calisync.CaliSync;
+import org.neatore.calisync.object.Date;
+
 import org.jetbrains.annotations.NotNull;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public final class HistoryParser {
+    public static JSONArray getHistory(@NotNull String dburl, @NotNull String it_unique_id) {
+        try (Connection conn = DriverManager.getConnection(dburl);
+             PreparedStatement pstmt = conn.prepareStatement("SELECT it_history FROM item_table WHERE it_unique_id = ?;")
+        ) {
+            pstmt.setString(1, it_unique_id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) return HistoryParser.decode(rs.getString("it_history"));
+        } catch (SQLException e) {
+            CaliSync.LOGGER.error(e);
+        }
+        return null;
+    }
+
+    public static JSONArray addHistory(@NotNull String dburl, @NotNull String it_unique_id, @NotNull String it_content) {
+        JSONArray array = Objects.requireNonNull(getHistory(dburl, it_unique_id));
+
+        JSONObject content = new JSONObject();
+        content.put("content", it_content);
+        content.put("time", Date.Now.getUnixTime());
+
+        array.put(content);
+        return array;
+    }
+
     public static String encode(@NotNull JSONArray history) {
         // 순서 정렬 (content -> time)
         List<Map<String, Object>> history_ = new ArrayList<>();
@@ -18,7 +52,7 @@ public final class HistoryParser {
             JSONObject obj = (JSONObject) o;
             Map<String, Object> map = new LinkedHashMap<>();
             map.put("content", obj.get("content"));
-            map.put("time", obj.get("time"));
+            map.put("time", obj.getLong("time"));
             history_.add(map);
         }
 
