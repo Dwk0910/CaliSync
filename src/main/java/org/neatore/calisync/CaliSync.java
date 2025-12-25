1a828f363127ddfab9434838766b2e63830aa08f
Dongwan Kim <dongwan204@naver.com>
2025-12-25 16:09:19 +0900

    Style: Cleaning code

--- src/main/java/org/neatore/calisync/CaliSync.java
+++ src/main/java/org/neatore/calisync/CaliSync.java
@@ -3,14 +3,8 @@ package org.neatore.calisync;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.util.List;
-import java.util.Objects;
-
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
-import org.neatore.calisync.object.Date;
-import org.neatore.calisync.object.Schedule;
 
 public class CaliSync {
     public static Logger LOGGER = LogManager.getLogger(CaliSync.class);
@@ -27,7 +21,5 @@ public class CaliSync {
 
         dbc = new DBC("jdbc:sqlite:" + database);
         LOGGER.info("Done.");
-
-        // Test
     }
 }
