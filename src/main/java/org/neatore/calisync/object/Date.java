d3c48bf9a4f9b90029bb44717038ebb2cb3c3040
Dongwan Kim <dongwan204@naver.com>
2025-12-25 22:31:17 +0900

    Data, Format and extra date type added

--- src/main/java/org/neatore/calisync/object/Date.java
+++ src/main/java/org/neatore/calisync/object/Date.java
@@ -1,6 +1,28 @@
 package org.neatore.calisync.object;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public record Date(String year, String month, String day) {
+    // ** DEFUALT ZONE ID **
+    private static final ZoneId timeZone = ZoneId.of("Asia/Seoul");
+
+    public static final class Now {
+        public static String getUnixTime() {
+            Instant instant = Instant.now();
+            return Long.toString(instant.atZone(timeZone).toEpochSecond());
+        }
+
+        public static String format(String format) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+            return Instant.ofEpochSecond(Long.parseLong(getUnixTime()))
+                    .atZone(timeZone)
+                    .format(formatter);
+        }
+    }
+
     private static String hour, minute, second;
     public void setTime(String hour, String minute, String second) {
         Date.hour = (hour.length() == 1) ? "0" + hour : hour;
@@ -10,11 +32,20 @@ public record Date(String year, String month, String day) {
 
     /**
      *
-     * @param type - Type of date string output.<br/>0 : returns `YYYY-MM-dd HH:mm:ss`<br/>1 : returns `YYYYMMdd`
+     * @param type - Type of date string output.<br/>0 : returns `YYYY-MM-dd HH:mm:ss`<br/>1 : returns `YYYYMMdd`<br/>2 : returns Linux Timestamp (EpochSecond)
      */
-    public String getDate(int type) {
+        public String getDate(int type) {
         if (type == 0) return "%s-%s-%s %s:%s:%s".formatted(year, month, day, hour, minute, second);
         else if (type == 1) return "%s%s%s".formatted(year, month, day);
+        else if (type == 2) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+            LocalDateTime localDateTime = LocalDateTime.parse("%s%s%s%s%s%s".formatted(year, month, day, hour, minute, second), formatter);
+            return Long.toString(
+                    localDateTime.atZone(timeZone)
+                    .toInstant()
+                    .getEpochSecond()
+            );
+        }
         else return null;
     }
 }
