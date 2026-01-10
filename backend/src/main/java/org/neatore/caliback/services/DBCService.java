287e97359ba065c5925661e611c6f66d98a2230b
Dongwan Kim <dongwan204@naver.com>
2026-01-10 23:59:06 +0900

    Feature: Null date handling, Added new methods

--- backend/src/main/java/org/neatore/caliback/services/DBCService.java
+++ backend/src/main/java/org/neatore/caliback/services/DBCService.java
@@ -1,5 +1,6 @@
 package org.neatore.caliback.services;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -9,8 +10,6 @@ import org.neatore.caliback.object.PacketResponse;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 import static org.neatore.caliback.CaliBack.dbc;
 
 @Service
@@ -19,14 +18,10 @@ public class DBCService {
             e400 = new PacketResponse(400, "Bad Request"),
             e500 = new PacketResponse(500, "Internal Server Error");
 
-    private static Map<String, Object> getData(JSONObject obj) throws JSONException {
-        return new JSONObject(obj.getString("data")).toMap();
-    }
-
     public PacketResponse process(JSONObject obj) {
         try {
-            Map<String, Object> data = getData(obj);
-            Date date = Date.parseDate(data.get("date").toString());
+            JSONObject data = obj.getJSONObject("data");
+            Date date = data.isNull("date") ? new Date("", "", "") : Date.parseDate(data.get("date").toString());
 
             switch (obj.getString("method")) {
                 case "POST" -> {
@@ -49,9 +44,17 @@ public class DBCService {
                     return new PacketResponse(200, dbc.getSchedulesAsJson(date));
                 }
 
-                default -> {
-                    return e404;
+                case "UPDATE_INFO" -> {
+                    return new PacketResponse(200, dbc.getUpdateInfo());
                 }
+
+                case "UPDATE" -> {
+                    JSONArray array = new JSONArray(data.getJSONArray("targets"));
+                    dbc.update(array);
+                    return new PacketResponse(200, null);
+                }
+
+                default -> { return e404; }
             }
         } catch (IllegalArgumentException | JSONException e) { return e400; }
           catch (Exception e) {
