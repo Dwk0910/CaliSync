b6567dcbb51d66b7a48f80fd74e15fe9365ab6b8
Dongwan Kim <dongwan204@naver.com>
2026-01-10 23:58:31 +0900

    Feature: Make Schedule general class

--- backend/src/main/java/org/neatore/caliback/object/Schedule.java
+++ backend/src/main/java/org/neatore/caliback/object/Schedule.java
@@ -1,4 +1,24 @@
 package org.neatore.caliback.object;
 
-public record Schedule(Date date, String content, boolean isCompleted) {
+import org.json.JSONObject;
+
+public class Schedule {
+    public final Date date;
+    public final String content;
+    public final boolean isCompleted;
+
+    public Schedule(Date date, String content) {
+        this.date = date;
+        this.content = content;
+        // TODO : 완료 여부 content 통해서 판단하기
+        this.isCompleted = false;
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject obj = new JSONObject();
+        obj.put("date", date.getDate(1));
+        obj.put("content", content);
+        obj.put("isCompleted", isCompleted);
+        return obj;
+    }
 }
