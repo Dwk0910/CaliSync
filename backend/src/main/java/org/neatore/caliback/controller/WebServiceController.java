5f431c31de4b3b9ca4833606b7cd35da6f26960a
Dongwan Kim <dongwan204@naver.com>
2026-01-25 19:26:03 +0900

    Todo added

--- backend/src/main/java/org/neatore/caliback/controller/WebServiceController.java
+++ backend/src/main/java/org/neatore/caliback/controller/WebServiceController.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.neatore.caliback.object.Date;
 import org.neatore.caliback.services.SpecialDayService;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:5173" })
 @RequestMapping("/webservice")
 public class WebServiceController {
     private final SpecialDayService specialDayService;
@@ -22,6 +24,7 @@ public class WebServiceController {
 
     @GetMapping("/specialdays/{year}/{month}")
     public ResponseEntity<String> getSpecialDays(@PathVariable String year, @PathVariable String month) {
+        // TODO: 이름 커스터마이징 가능하도록 (예 1월 1일 -> 신정)
         JSONArray serviceResult = new JSONArray(specialDayService.getSpecialDays(new Date(year, month, "0"))
                 .stream()
                 .map(
