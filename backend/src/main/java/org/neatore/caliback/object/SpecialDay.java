e1942b27c7b8ed4c4c30caeed258864ed9c0df80
Dongwan Kim <dongwan204@naver.com>
2026-01-25 19:26:23 +0900

    Fix: special day naming comparing

--- backend/src/main/java/org/neatore/caliback/object/SpecialDay.java
+++ backend/src/main/java/org/neatore/caliback/object/SpecialDay.java
@@ -9,11 +9,12 @@ public record SpecialDay(String name, String date, String type) {
         if (!(o instanceof SpecialDay o_)) return false;
 
         // type을 비교하면 날짜가 중복되더라도 type이 달라서 Set에 그대로 들어갈 수 있으므로 비교하면 안됨
-        return o_.name.equals(this.name) && o_.date.equals(this.date);
+        // 어린이 날 -> 어린이날
+        return o_.name.replaceAll("\\s+", "").equalsIgnoreCase(this.name.replaceAll(" ", "")) && o_.date.equals(this.date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, date);
+        return Objects.hash(name.replaceAll("\\s+", ""), date);
     }
 }
