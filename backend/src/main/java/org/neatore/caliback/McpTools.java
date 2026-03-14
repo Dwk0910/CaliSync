539c084db83ebd3b36e9386f870de3f2e8fcc4a3
Dongwan Kim <dongwan204@naver.com>
2026-03-14 20:37:18 +0900

    Feature: McpTool annotation needs more information

--- backend/src/main/java/org/neatore/caliback/McpTools.java
+++ backend/src/main/java/org/neatore/caliback/McpTools.java
@@ -3,6 +3,7 @@ package org.neatore.caliback;
 import lombok.RequiredArgsConstructor;
 
 import org.neatore.caliback.services.DBCService;
+import org.neatore.caliback.services.UserVerifyService;
 
 import org.springaicommunity.mcp.annotation.McpTool;
 import org.springaicommunity.mcp.annotation.McpToolParam;
@@ -13,9 +14,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class McpTools {
     private final DBCService dbcService;
+    private final UserVerifyService uvs;
 
-    @McpTool(description = "Calisync에서 특정 날짜의 일정을 모두 가져옵니다.")
-    public String getEvents(@McpToolParam(description = "가져올 일정의 날짜를 yyyy.MM.dd.의 형식으로 작성") String date) {
-        return "일정이 없습니다.";
+    @McpTool(name = "Authorize", description = "CaliSync의 사용자 인증을 진행합니다.")
+    public String authorize(@McpToolParam(description = "Google authorization code") String code) {
+        return "";
+    }
+
+    @McpTool(name = "Get-schedules", description = "CaliSync에서 특정 날짜의 일정을 모두 가져옵니다.")
+    public String getSchedules(@McpToolParam(description = "가져올 일정의 날짜를 yyyy.MM.dd.의 형식으로 작성") String date) {
+        return "";
     }
 }
