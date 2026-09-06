a19fc63d10b6669a7ca83a8af6da32b4491c5035
Dongwan Kim <dongwan204@naver.com>
2026-09-06 16:10:08 +0900

    Feature: Made server url editable

--- client/src/main/java/org/neatore/calisync/CaliSync.java
+++ client/src/main/java/org/neatore/calisync/CaliSync.java
@@ -6,24 +6,38 @@ import org.apache.logging.log4j.Logger;
 import org.neatore.calisync.util.CalendarProcess;
 import org.neatore.calisync.util.NotifySystem;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import java.util.Arrays;
 
 public class CaliSync {
 
+    public static Logger LOGGER = LogManager.getLogger(CaliSync.class);
+
     public static final Path defaultDir = Path.of(System.getProperty("user.home"), "AppData", "Roaming", "CalendarTask");
+    public static final Path urlPath = Path.of(System.getProperty("user.home"), "AppData", "Roaming", "Calisync", "url.txt");
     public static final Path process = Path.of(defaultDir.toString(), "desktopcal.exe");
 
     public static final Path dbDir = Path.of(defaultDir.toString(), "Db");
     public static final Path dbPath = Path.of(dbDir.toString(), "calendar.db");
 
     public static final String dburl = "jdbc:sqlite:" + dbPath;
-    public static final String serverurl = "neatorebackend.kro.kr/calisync";
-    public static final String serverurl_local = "localhost:8080";
+    public static final String serverurl;
+
+    static {
+        try {
+            serverurl = Files.readString(urlPath);
+        } catch (IOException e) {
+            LOGGER.fatal("Could not read server URL from {}", urlPath.toString(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final String serverurl_local = "http://localhost:8080";
     public static String url;
 
-    public static Logger LOGGER = LogManager.getLogger(CaliSync.class);
     public static NotifySystem notifySystem = new NotifySystem();
 
     public static void main(String[] args) {
@@ -35,7 +49,7 @@ public class CaliSync {
         LOGGER.info("Process started. Opening connection to the CaliSync backend SSE Server...");
 
         boolean localtest = Arrays.stream(args).toList().contains("-LOCALTEST");
-        CaliSync.url = ((localtest) ? "http://" + serverurl_local : "https://" + serverurl);
+        CaliSync.url = ((localtest) ? serverurl_local : serverurl);
 
         SSEClient client = new SSEClient();
         client.start();
